a5b145635f57e9c4ce6dbfbdbfe95d10e41c6682
Lay <lay.merabti@gmail.com>
2024-01-26 17:22:25 +0100

    ariane

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/year', function(req, res, next) {
-//   const date = new Date().getFullYear();
-//   res.json({ year: date});
-// });
-
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 
 module.exports = router;
--- routes/year.js
+++ routes/year.js
@@ -2,11 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/year', function(req, res, next) {
+router.get('/', function( req, res, next) {
   const year = new Date().getFullYear();
   res.json({year});
 });
-
-
 
 module.exports = router;
